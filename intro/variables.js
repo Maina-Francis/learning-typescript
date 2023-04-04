040f8ec21ad246b29534ff52ee9818610dafa309
Francis Maina <sebastianfrancix@gmail.com>
2023-04-04 07:19:56 +0300

    learnt about string, number and boolean and type inference in TS

---
+++ intro/variables.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Francis";
+console.log(greetings);
+// number (includes also floats)
+var userId = 334455;
+var age = 23; //Type inference
+// boolean
+var isLoggedIn = false;
+var isLoggedOut = true; //also valid
---
+++ intro/variables.ts
@@ -0,0 +1,13 @@
+let greetings: string = "Hello Francis";
+
+console.log(greetings);
+
+// number (includes also floats)
+let userId: number = 334455;
+let age = 23; //Type inference
+
+// boolean
+let isLoggedIn: boolean = false;
+let isLoggedOut = true; //also valid
+
+export {};
