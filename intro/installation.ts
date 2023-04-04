a0fa477122e92bd0e42597b8dd22f6c03019237a
Francis Maina <sebastianfrancix@gmail.com>
2023-04-04 06:35:32 +0300

    covered how to install Typescript in our machine globally and in development mode

---
+++ intro/installation.ts
@@ -0,0 +1,5 @@
+// To install typescript globally, use npm install -g typescript
+
+// To install Typescript in our development environment, use: npm install typescript --save-dev
+
+// To check the version of Typescript, use: tsc -v
