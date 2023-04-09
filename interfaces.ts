e3a3e41ae3014926b77c64fd64f284d03d3df5fc
Francis Maina <sebastianfrancix@gmail.com>
2023-04-09 18:12:28 +0300

    covered interfaces in TS

---
+++ interfaces.ts
@@ -0,0 +1,42 @@
+interface User {
+  name: string;
+  userId: number;
+  readonly dbId: number;
+  googleId?: string;
+  //   startTrial: () => string;
+  startTrial(): string;
+}
+
+// const frank: User = {
+//   name: "Frank",
+//   userId: 20,
+//   dbId: 2,
+//   startTrial: () => "Trial Started",
+// };
+
+//Reopening/ extending the interface
+interface User {
+  githubToken: string;
+}
+
+const frank: User = {
+  name: "Frank",
+  userId: 20,
+  dbId: 2,
+  githubToken: "github",
+  startTrial: () => "Trial Started",
+};
+
+// Extend the interface
+interface Admin extends User {
+  role: "admin" | "ta";
+}
+
+const maina: Admin = {
+  role: "admin",
+  name: "maina",
+  userId: 240,
+  dbId: 24,
+  githubToken: "github",
+  startTrial: () => "Trial Started by Admin",
+};
