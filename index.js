399ebb71f1fbf69daa19e259ec79446e023cae1c
Francis Maina <sebastianfrancix@gmail.com>
2023-04-14 14:47:47 +0300

    covered private and public properties

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 "use strict";
-class Users {
-    constructor(email, name) {
-        this.city = "";
+// class Users {
+//   email: string;
+//   name: string;
+//   private city: string = ""; //   private keyword makes the property inaccessible outside the class. Everything else is public
+Object.defineProperty(exports, "__esModule", { value: true });
+//   private city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// Professional way of writing it:
+var Users = /** @class */ (function () {
+    function Users(email, name) {
         this.email = email;
         this.name = name;
+        this.city = "Nairobi";
     }
-}
-const frank = new Users("f@f,com", "Frank");
+    return Users;
+}());
+var frank = new Users("f@f,com", "Frank");
+frank.city;
--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
+// class Users {
+//   email: string;
+//   name: string;
+//   private city: string = ""; //   private keyword makes the property inaccessible outside the class. Everything else is public
+
+//   private city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// Professional way of writing it:
 class Users {
-  email: string;
-  name: string;
-  city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  private city: string = "Nairobi";
+  constructor(public email: string, public name: string) {}
 }
 
 const frank = new Users("f@f,com", "Frank");
+frank.city;
 
 export {};
