42739c556e158dfbdf563cd9e39bcd0245c4e031
Francis Maina <sebastianfrancix@gmail.com>
2023-04-14 15:00:03 +0300

    covered access modifiers: Public, Private and Protected in ts

--- index.ts
+++ index.ts
@@ -13,10 +13,23 @@
 // Professional way of writing it:
 class Users {
   private city: string = "Nairobi";
+  protected courseCount: number = 1;
   constructor(public email: string, public name: string) {}
 }
 
 const frank = new Users("f@f,com", "Frank");
-frank.city;
+// frank.city;
 
+// inherit a class, but can't inherit private modifiers
+class SubUser extends Users {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this.courseCount = 4;
+  }
+}
+
+// Access modifiers recap
+// Private - Only accessible within the class
+// Public - accessible from anywhere
+// protected - accessible within the class and any class that inherits it but not outside those parameters
 export {};
